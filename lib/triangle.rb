469901977ccf7fb9eab62c7893daf5439594eeaf
samanthamarberger <slatham04@gmail.com>
2023-03-17 21:24:25 -0600

    commit tree e94ce18ad6b66c2e7ec071a28bd9fff072d386bf

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  attr_reader :s_one, :s_two, :s_three
+
+  def initialize(s_one, s_two, s_three)
+    @s_one = s_one
+    @s_two = s_two
+    @s_three = s_three
+  end
+
+  def is_valid
+    if self.s_one > 0 && self.s_two > 0 && self.s_three > 0
+      if self.s_one + self.s_two > self.s_three && self.s_three + self.s_two > self.s_one && self.s_one + self.s_three > self.s_two
+        return true
+      else
+        return false
+    end
+  end
+
+  def kind
+    if is_valid
+      if self.s_one == self.s_two && self.s_one == self.s_three
+        return "equilateral"
+      elsif self.s_one === self.s_two || self.s_one == self.s_three || self.s_two == self.s_three
+        return "isosceles"
+      else
+        return "scalene"
+      end
+    else
+      raise TriangleError
+  end
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
